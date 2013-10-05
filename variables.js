c85b41d9970e34bf3f5e0e2f4d10b24a7273f2c3
Krysten Yates <kjyates@gmail.com>
2013-10-05 15:40:44 -0700

    add first assignment

---
+++ variables.js
@@ -0,0 +1,33 @@
+var x = 7
+
+var y = 12
+
+var z = 3
+
+var total = (x + y) * z
+
+console.log(total)
+
+var cat = 'Lily'
+
+var dog = 'Jordy'
+
+var pets = cat + ' and ' + dog
+
+console.log(pets)
+
+var boy = 'Chris'
+
+var girl = 'Krysten'
+
+var humans = boy + ' and ' + girl
+
+console.log(humans)
+
+var household = humans + ' & ' + pets
+
+console.log(household)
+
+var random = household + ' ; ' + total
+
+console.log(random)
